bb2f51c693762e35e44d202ffdf988f1d80da27c
Ashley Tucker <ashleyst35@aol.com>
2022-09-15 14:20:38 -0400

    work

--- app.js
+++ app.js
@@ -5,3 +5,86 @@ console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+// let i = 0
+// for(let i = 0; i < 100; i++) {
+//     if (i % 2== 0) {
+//         continue;
+//     }
+//     else{
+//         console.log(i)
+//     }
+//   // loged the odd number^
+
+  //fizzbuzz #2
+//   for(let i =1; i < 100; i++) {
+
+//     if ( i % 3 == 0 && i % 5 == 0) {
+//         console.log("fizzzzBuzzzz");
+
+//     } else {
+
+//     if (i % 3 == 0) {
+//         console.log("FIZZZZ");
+//        }
+    
+//      if (i % 5 == 0 ) {
+//         console.log("Bizzzz");
+//      }  else{
+//         console.log(i);
+//      }
+//     }    
+
+//   }
+
+
+  //Exercise #3 while/Do
+  // let i = 1;
+
+  // while(i <=100){
+  //   let output = "";
+
+  //   if (i % 3 == 0) {
+  //       output += "Fizzz";
+  //   }
+
+  //   if (i % 5 == 0){
+  //       output +="Buzz";
+    
+  //   }
+  //    console.log (` ${i} ${output}`);
+  //    i++;
+
+  // }
+
+  //Exercise #4
+  // let findNumber = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+  // let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+  
+  // for (let i = 1; i <= n; i++){
+  //   //console.log(i); // i is findNumber*
+  //  if(i == findNumber) {
+  //   console.log(`Found ${findNumber}!`);
+  //   break; // break loop at number fond console says "found" at the random number
+  //  }
+    
+  // }
+  //  console.log(`No number found ${findNumber} number is within 1-${n}`);
+
+
+
+
+//Exercise #5
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let a = Math.round(Math.random() * (1000 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+
+
+
+for ( let i = start; i <= end; i++) {
+  console.log(i);
+}
+
+
